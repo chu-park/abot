ebfbfe9ee1033592ec624683b8a11f8b8aa86878
chu-park <sa55797202@gmail.com>
2022-10-18 21:31:09 +0900

    1
    1

--- item1.py
+++ item1.py
@@ -1,11 +1,20 @@
-item2_name = "붕괴된 마력의 정수"
-item2_s_name = "붕마정"
-item2_i1 = "파괴적인 마력이 담긴 오래된 정수. 불균형을 이용해 오히려 더 큰 파괴력을 발휘하게 되었다. 제작 재료로 사용하거나 분해 시 유용한 아이템을 얻을 수 있다."
-item2_i2 = "키아 상급 하드, 알비 상급 하드, 룬다 상급 하드"
-item2_i3 = "⭐⭐⭐⭐⭐"
-item2_i4 = "https://cdn.discordapp.com/attachments/740130260183679008/1031901400508141618/unknown.png"
+j_item1_name = "붕괴된 마력의 정수"
+j_item1_s_name = "붕마정"
+j_item1_i1 = "파괴적인 마력이 담긴 오래된 정수. 불균형을 이용해 오히려 더 큰 파괴력을 발휘하게 되었다. 제작 재료로 사용하거나 분해 시 유용한 아이템을 얻을 수 있다."
+j_item1_i2 = "키아 상급 하드, 알비 상급 하드, 룬다 상급 하드"
+j_item1_i3 = "⭐⭐⭐⭐⭐"
+j_item1_i4 = "https://cdn.discordapp.com/attachments/740130260183679008/1031901400508141618/unknown.png"
+
+j_item2_name = "변형된 고리형 구조체"
+j_item2_s_name = "변고구"
+j_item2_i1 = "마법적인 의식과 관계가 있는 것으로 보이는 고리형 구조체. 변형되었지만 강력한 힘을 담을 수 있을 것 같다. 제작 재료로 사용하거나 분해 시 유용한 아이템을 얻을 수 있다."
+j_item2_i2 = "각종 던전"
+j_item2_i3 = "⭐⭐"
+j_item2_i4 = "https://cdn.discordapp.com/attachments/740130260183679008/1031907198508470292/unknown.png"
 
 a1 = {
-        item2_name : { "b1": item2_name, "c1": item2_i1, "d1": item2_i2, "e1": item2_i3, "f1": item2_i4},
-        item2_s_name : { "b1": item2_name, "c1": item2_i1, "d1": item2_i2, "e1": item2_i3, "f1": item2_i4}
+        j_item1_name : { "b1": j_item1_name, "c1": j_item1_i1, "d1": j_item1_i2, "e1": j_item1_i3, "f1": j_item1_i4},
+        j_item1_s_name : { "b1": j_item1_name, "c1": j_item1_i1, "d1": j_item1_i2, "e1": j_item1_i3, "f1": j_item1_i4},
+        j_item2_name : { "b1": j_item2_name, "c1": j_item2_i1, "d1": j_item2_i2, "e1": j_item2_i3, "f1": j_item2_i4},
+        j_item2_s_name : { "b1": j_item2_name, "c1": j_item2_i1, "d1": j_item2_i2, "e1": j_item2_i3, "f1": j_item2_i4}
 }
