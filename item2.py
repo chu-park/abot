13a522f9c1ee74060f933ab5d0fbc04920ac69ea
chu-park <sa55797202@gmail.com>
2022-10-19 22:04:25 +0900

    2
    2

--- item2.py
+++ item2.py
@@ -15,7 +15,27 @@ in_item1_i12 = ""
 in_item1_i13 = ""
 in_item1_i14 = "```"
 
+in_item2_name = "가리워진"
+in_item2_s_name = "-"
+in_item2_i1 = "접두 F 랭크"
+in_item2_i2 = "입수정보가 입력되어있지 않습니다."
+in_item2_i3 = "팔라라의 조각에 인챈트 가능"
+in_item2_i4 = "-"
+in_item2_i5 = "```diff\n-인챈트 장비를 전용으로 만듦"
+in_item2_i6 = "레벨이 10 이상일때 최대대미지 3 증가"
+in_item2_i7 = "최대생명력 5 증가"
+in_item2_i8 = "100% 인챈트 성공"
+in_item2_i9 = ""
+in_item2_i10 = ""
+in_item2_i11 = ""
+in_item2_i12 = ""
+in_item2_i13 = ""
+in_item2_i14 = "```"
+
 a2 = {
         in_item1_name : { "b2": in_item1_name, "c2": in_item1_i1, "d2": in_item1_i2, "e2": in_item1_i3, "f2": in_item1_i4, "g2": in_item1_i5, "h2": in_item1_i6, "i2": in_item1_i7, "j2": in_item1_i8, "k2": in_item1_i9, "l2": in_item1_i10, "m2": in_item1_i11, "n2": in_item1_i12, "o2": in_item1_i13, "p2": in_item1_i14},
-        in_item1_s_name : { "b2": in_item1_name, "c2": in_item1_i1, "d2": in_item1_i2, "e2": in_item1_i3, "f2": in_item1_i4, "g2": in_item1_i5, "h2": in_item1_i6, "i2": in_item1_i7, "j2": in_item1_i8, "k2": in_item1_i9, "l2": in_item1_i10, "m2": in_item1_i11, "n2": in_item1_i12, "o2": in_item1_i13, "p2": in_item1_i14}
+        in_item1_s_name : { "b2": in_item1_name, "c2": in_item1_i1, "d2": in_item1_i2, "e2": in_item1_i3, "f2": in_item1_i4, "g2": in_item1_i5, "h2": in_item1_i6, "i2": in_item1_i7, "j2": in_item1_i8, "k2": in_item1_i9, "l2": in_item1_i10, "m2": in_item1_i11, "n2": in_item1_i12, "o2": in_item1_i13, "p2": in_item1_i14},
+        in_item2_name : { "b2": in_item2_name, "c2": in_item2_i1, "d2": in_item2_i2, "e2": in_item2_i3, "f2": in_item2_i4, "g2": in_item2_i5, "h2": in_item2_i6, "i2": in_item2_i7, "j2": in_item2_i8, "k2": in_item2_i9, "l2": in_item2_i10, "m2": in_item2_i11, "n2": in_item2_i12, "o2": in_item2_i13, "p2": in_item2_i14},
+        in_item2_s_name : { "b2": in_item2_name, "c2": in_item2_i1, "d2": in_item2_i2, "e2": in_item2_i3, "f2": in_item2_i4, "g2": in_item2_i5, "h2": in_item2_i6, "i2": in_item2_i7, "j2": in_item2_i8, "k2": in_item2_i9, "l2": in_item2_i10, "m2": in_item2_i11, "n2": in_item2_i12, "o2": in_item2_i13, "p2": in_item2_i14}
+
 }
