37135a6195f4d18d1be18ebb65fc825f518ff4e7
chu-park <sa55797202@gmail.com>
2022-10-19 20:37:23 +0900

    1
    1

--- item1.py
+++ item1.py
@@ -8,13 +8,49 @@ j_item1_i4 = "https://cdn.discordapp.com/attachments/740130260183679008/10319014
 j_item2_name = "변형된 고리형 구조체"
 j_item2_s_name = "변고구"
 j_item2_i1 = "마법적인 의식과 관계가 있는 것으로 보이는 고리형 구조체. 변형되었지만 강력한 힘을 담을 수 있을 것 같다. 제작 재료로 사용하거나 분해 시 유용한 아이템을 얻을 수 있다."
-j_item2_i2 = "각종 던전"
+j_item2_i2 = "룬다 상급 하드 던전, 알비 상급 하드 던전, 키아 상급 하드 던전, 몽환의 라비 던전 등 "
 j_item2_i3 = "⭐⭐⭐"
 j_item2_i4 = "https://cdn.discordapp.com/attachments/740130260183679008/1031907198508470292/unknown.png"
+
+j_item3_name = "마력의 정수"
+j_item3_s_name = "마정"
+j_item3_i1 = "파괴적인 마력이 담긴 오래된 정수. 제작 재료로 사용하거나 분해시 유용한 아이템을 얻을 수 있다. "
+j_item3_i2 = "룬다 상급 하드 던전, 알비 상급 하드 던전, 몽환의 라비 던전 등"
+j_item3_i3 = "⭐⭐"
+j_item3_i4 = "https://cdn.discordapp.com/attachments/740130260183679008/1032044388777463848/unknown.png"
+
+j_item4_name = "깨어난 힘의 정수"
+j_item4_s_name = "깨정"
+j_item4_i1 = "이제부터 시작될 싸움을 위해 마침내 눈을 뜬 전사의 힘을 담은 정수. 특별한 무기의 재료로 사용된다."
+j_item4_i2 = "울라 상급 던전, 알비·키아·룬다 상급 하드 던전  등 "
+j_item4_i3 = "⭐⭐"
+j_item4_i4 = "https://cdn.discordapp.com/attachments/740130260183679008/1032047531636576346/unknown.png"
+
+j_item5_name = "정화된 사도의 정수"
+j_item5_s_name = "정사정"
+j_item5_i1 = "이질적인 신성력에 의해 고통받던 원념이 깨끗이 정화된 사도의 정수. 무기의 에르그 개방에 사용할 수 있을 것 같다. "
+j_item5_i2 = "제작: 매직 크래프트(1단)- 신성한 광물 조각 10개, 부패한 사도의 가죽 5개, 사도의 정수 1개"
+j_item5_i3 = "⭐⭐⭐⭐⭐"
+j_item5_i4 = "https://cdn.discordapp.com/attachments/740130260183679008/1032051280937488545/unknown.png"
+
+j_item6_name = "사도의 정수"
+j_item6_s_name = "사정"
+j_item6_i1 = "위험한 기운을 내뿜는 사도의 정수. 사도의 원념으로 인해 오염되어 있다. 에르그 개방에 필요한 재료를 만드는 데 사용할 수 있을 것 같다. 분해 시에도 유용한 아이템을 얻을 수 있다. "
+j_item6_i2 = "하시딤 레이드 보상"
+j_item6_i3 = "⭐⭐⭐"
+j_item6_i4 = "https://cdn.discordapp.com/attachments/740130260183679008/1032050005348986951/unknown.png"
 
 a1 = {
         j_item1_name : { "b1": j_item1_name, "c1": j_item1_i1, "d1": j_item1_i2, "e1": j_item1_i3, "f1": j_item1_i4},
         j_item1_s_name : { "b1": j_item1_name, "c1": j_item1_i1, "d1": j_item1_i2, "e1": j_item1_i3, "f1": j_item1_i4},
         j_item2_name : { "b1": j_item2_name, "c1": j_item2_i1, "d1": j_item2_i2, "e1": j_item2_i3, "f1": j_item2_i4},
-        j_item2_s_name : { "b1": j_item2_name, "c1": j_item2_i1, "d1": j_item2_i2, "e1": j_item2_i3, "f1": j_item2_i4}
+        j_item2_s_name : { "b1": j_item2_name, "c1": j_item2_i1, "d1": j_item2_i2, "e1": j_item2_i3, "f1": j_item2_i4},
+        j_item3_name : { "b1": j_item3_name, "c1": j_item3_i1, "d1": j_item3_i2, "e1": j_item3_i3, "f1": j_item3_i4},
+        j_item3_s_name : { "b1": j_item3_name, "c1": j_item3_i1, "d1": j_item3_i2, "e1": j_item3_i3, "f1": j_item3_i4},
+        j_item4_name : { "b1": j_item4_name, "c1": j_item4_i1, "d1": j_item4_i2, "e1": j_item4_i3, "f1": j_item4_i4},
+        j_item4_s_name : { "b1": j_item4_name, "c1": j_item4_i1, "d1": j_item4_i2, "e1": j_item4_i3, "f1": j_item4_i4},
+        j_item5_name : { "b1": j_item5_name, "c1": j_item5_i1, "d1": j_item5_i2, "e1": j_item5_i3, "f1": j_item5_i4},
+        j_item5_s_name : { "b1": j_item5_name, "c1": j_item5_i1, "d1": j_item5_i2, "e1": j_item5_i3, "f1": j_item5_i4},
+        j_item6_name : { "b1": j_item6_name, "c1": j_item6_i1, "d1": j_item6_i2, "e1": j_item6_i3, "f1": j_item6_i4},
+        j_item6_s_name : { "b1": j_item6_name, "c1": j_item6_i1, "d1": j_item6_i2, "e1": j_item6_i3, "f1": j_item6_i4}
 }
